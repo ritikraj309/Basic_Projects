b9417a61d3b8edd2b2a51b0783945425b722cca8
ritikraj309 <142624647+ritikraj309@users.noreply.github.com>
2023-09-18 23:34:28 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="deleteBtn">Delete</button>
+        `;
+
+        taskList.appendChild(li);
+        taskInput.value = "";
+
+        const deleteBtn = li.querySelector(".deleteBtn");
+        deleteBtn.addEventListener("click", () => {
+            taskList.removeChild(li);
+        });
+    }
+}
